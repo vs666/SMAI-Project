4abbad872525f31668a5ec6b8303aca0c76a1cb4
vs666 <varul.srivastava@research.iiit.ac.in>
2021-12-07 01:52:38 +0530

    added script for testing rusboost

---
+++ code/RusBoost/tmp.sh
@@ -0,0 +1,20 @@
+echo
+echo "Imbalance : 2:98"
+echo
+python3 rusboost.py DecisionTree 0.02 4 10000 30 0.3
+echo
+echo "Imbalance : 4:96"
+echo
+python3 rusboost.py DecisionTree 0.04 4 10000 30 0.3
+echo
+echo "Imbalance : 8:92"
+echo
+python3 rusboost.py DecisionTree 0.08 4 10000 30 0.3
+echo
+echo "Imbalance : 16:84"
+echo
+python3 rusboost.py DecisionTree 0.16 4 10000 30 0.3
+echo
+echo "Imbalance : 32:68"
+echo
+python3 rusboost.py DecisionTree 0.32 4 10000 30 0.3
